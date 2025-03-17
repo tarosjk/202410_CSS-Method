66e6311d3c7f4a54039df068686473a4264f5f59
taro shinjyuku <tarosjk@gmail.com>
2025-03-17 12:52:47 +0900

    メインビジュアルのスライドショー導入

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,22 @@ $(function () {
     // toggleClass(指定したクラスを追加・削除)
     $('body').toggleClass('is-openMenu')
   })
-})
+});
+
+(function () {
+  'use strict'
+
+  new Swiper('#kv', {
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+      delay: 4000,
+      pauseOnMouseEnter: true
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  })
+})()
+
+
