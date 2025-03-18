4510ba0317f3788d5dbd186df0cdc405d8fe3e01
taro shinjyuku <tarosjk@gmail.com>
2025-03-18 10:23:35 +0900

    商品詳細のスライドの追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ $(function () {
 (function () {
   'use strict'
 
+  // メインビジュアル
   new Swiper('#kv', {
     loop: true,
     effect: 'fade',
@@ -21,6 +22,32 @@ $(function () {
       el: '.swiper-pagination',
     }
   })
+
+
+  // ===============================
+  // 商品詳細スライダー
+  // サムネイル
+  const galleryThumbs = new Swiper('#gallery-thumbs', {
+    // slidesPerView: 3.5 //端数を入れてもOK
+    slidesPerView: 'auto',
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  })
+
+  // メインスライド
+  new Swiper('#detail-slide', {
+    loop: true,
+    effect: 'fade',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  })
+
 })()
 
 
